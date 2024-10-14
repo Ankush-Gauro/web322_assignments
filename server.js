1cb714fe4637af487141efb9763cb45ac9d625a0
Ankush Gauro <agauro@mySeneca.ca>
2024-10-14 03:18:44 -0400

    AS2 P5

--- server.js
+++ server.js
@@ -21,3 +21,37 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
+// Route for getting articles
+app.get('/articles', (req, res) => {
+  contentService.getAllArticles()
+      .then((data) => {
+          res.json(data); // Send the articles data
+      })
+      .catch((err) => {
+          res.json({ message: err }); // Send error message 
+      });
+});
+
+// Route for getting categories
+app.get('/categories', (req, res) => {
+  contentService.getCategories()
+      .then((data) => {
+          res.json(data); // Send the categories data 
+      })
+      .catch((err) => {
+          res.json({ message: err }); // Send error message 
+      });
+});
+
+// Initialize the content service and start the server
+contentService.initialize()
+  .then(() => {
+      app.listen(HTTP_PORT, () => {
+          console.log(`Express http server listening on port ${HTTP_PORT}`);
+      });
+  })
+  .catch((err) => {
+      console.log(`Failed to initialize content service: ${err}`);
+  });
+
+module.exports = app;
