0d81ef3051fa2dc71ec3f905a2e57bcfa9331928
Ankush Gauro <agauro@mySeneca.ca>
2024-10-14 03:16:05 -0400

    AS2 P2S4

--- content-service.js
+++ content-service.js
@@ -1,15 +1,81 @@
+
+// Ankush Gauro : 108593237 
 const fs = require('fs');
 const path = require('path');
 
+let articles = [];
+let categories = [];
 
+// This function will read the contents of the "./data/articles.json" file.
 const initialize = () => {
-    
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, 'data', 'articles.json'), 'utf8', (err, data) => {
+            if (err) {
+                reject('unable to read articles file');
+                return;
+            }
+
+            try {
+                articles = JSON.parse(data);
+            } catch (parseErr) {
+                reject('unable to parse articles JSON');
+                return;
+            }
+        });
+
+            fs.readFile(path.join(__dirname, 'data', 'categories.json'), 'utf8', (err, data) => {
+                if (err) {
+                    reject('unable to read categories file');
+                    return;
+                }
+
+                try {
+                    categories = JSON.parse(data);
+                    resolve();
+                } catch (parseErr) {
+                    reject('unable to parse categories JSON');
+                }
+            });
+    });
 };
 
+// This function will provide the full array of "articles" object whose published property is ‘true’, using the resolve method of the returned promise.
 const getPublishedArticles = () => {
-    
+    return new Promise((resolve, reject) => {
+        const publishedArticles = articles.filter(article => article.published === true);
+        if (publishedArticles.length > 0) {
+            resolve(publishedArticles);
+        } else {
+            reject('no results returned');
+        }
+    });
 };
 
+// This function will provide an array of all “articles" objects
+const getAllArticles = () => {
+    return new Promise((resolve, reject) => {
+        if (articles.length > 0) {
+            resolve(articles);
+        } else {
+            reject('no results returned');
+        }
+    });
+};
+
+// This function will provide the full array of "category" objects 
 const getCategories = () => {
-    
+    return new Promise((resolve, reject) => {
+        if (categories.length > 0) {
+            resolve(categories);
+        } else {
+            reject('no results returned');
+        }
+    });
+};
+
+module.exports = {
+    initialize,
+    getPublishedArticles,
+    getAllArticles,
+    getCategories
 };
--- server.js
+++ server.js
@@ -1,51 +1,23 @@
+// Ankush Gauro - 108593237
+const express = require('express');
+const path = require('path');
+const contentService = require('./content-service'); // Import the content-service module
 
+const app = express();
+const HTTP_PORT = 8080;
 
-const express = require('express'); // "require" the Express module
-
-const path = require('path'); // path
-const app = express(); // obtain the "app" object
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
 app.use(express.static('public'));
 
-
-//Get route for index
-
+// Route handlers
 app.get('/', (req, res) => {
-  res.redirect('/about');
+    res.redirect('/about');
 });
 
 app.get('/home', (req, res) => {
-  res.redirect('/about');
+    res.redirect('/about');
 });
 
-
-//Get route for about
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/about.html'));
-  });
-
-  app.get('/articles', (req, res) => {
-    contentService.getAllArticles()
-        .then((data) => {
-            res.json(data); // Send the articles data
-        })
-        .catch((err) => {
-            res.json({ message: err }); // Send error message 
-        });
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
-
-// Route for getting categories
-app.get('/categories', (req, res) => {
-    contentService.getCategories()
-        .then((data) => {
-            res.json(data); // Send the categories data 
-        })
-        .catch((err) => {
-            res.json({ message: err }); // Send error message 
-        });
-});
-
-
-// start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () => console.log(`Express http server listening on port ${HTTP_PORT}`));
 
