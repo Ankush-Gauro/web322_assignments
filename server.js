8056ea9acccf41c5b724ba274d12b5fe4f78a25f
Ankush Gauro <agauro@mySeneca.ca>
2024-10-14 02:44:38 -0400

    AS2 P1S4

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const express = require('express'); // "require" the Express module
 const path = require('path'); // path
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
+app.use(express.static('public'));
+
 
 //Get route for index
+
+app.get('/', (req, res) => {
+  res.redirect('/about');
+});
+
+app.get('/home', (req, res) => {
+  res.redirect('/about');
+});
+
 
 //Get route for about
 app.get('/about', (req, res) => {
@@ -15,5 +26,5 @@ app.get('/about', (req, res) => {
 
 
 // start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+app.listen(HTTP_PORT, () => console.log(`Express http server listening on port ${HTTP_PORT}`));
 
